29fc6053f010534cdf40f496e17046d972115afc
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2024-03-12 11:29:00 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ('Mr. Chocolate', 13, M, 'brown', 'high', 0),
+    ('Rowdy', 19, F, 'black', 'high', 1),
+    ('Tabitha', 23, F, 'white', 'low', 0),
+    ('Sergeant Brown', 18, M, 'brown', 'low', 1),
+    ('Melissa', 21, F, 'gold', 'high', 1),
+    ('Grinch', 23, M, 'green', 'low', 0),
+    ('Wendy', 20, F, 'black', 'high', 1),
+    (NULL, 21, M, 'grey', 'high', 0);
